b616672c4b02b0065295cce21ebd11ef2b944b08
Adan0104 <abidinadanul@gmail.com>
2025-11-11 23:49:31 +0530

    train service updations

--- src/main/java/ticket/booking/system/services/TrainService.java
+++ src/main/java/ticket/booking/system/services/TrainService.java
@@ -1,9 +1,14 @@
 package ticket.booking.system.services;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import ticket.booking.system.entities.Train;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class TrainService {
 
@@ -11,7 +16,30 @@ public class TrainService {
     private ObjectMapper objectMapper = new ObjectMapper();
     private static final String TRAIN_DB_PATH = "../localDb/trains.json";
 
-    public TrainService(){
-        File
+    public TrainService() throws IOException {
+        File trains = new File(TRAIN_DB_PATH);
+        trainList = objectMapper.readValue(trains, new TypeReference<List<Train>>() {});
+    }
+
+    private void saveTrainListToFile(){
+        try{
+            objectMapper.writeValue(new File(TRAIN_DB_PATH),trainList);
+        } catch (IOException e) {
+            System.err.println("Train details could not be saved");
+            e.printStackTrace();
+        }
+    }
+
+    public List<Train> searchTrains(String source, String destination) {
+        return trainList.stream().filter(train -> validTrain(train, source, destination)).collect(Collectors.toList());
+    }
+
+    public boolean validTrain(Train train,String source,String destination){
+        List<String> stationOrder = train.getStations();
+
+        int sourceStation = stationOrder.indexOf(source);
+        int destinationStation = stationOrder.indexOf(destination);
+
+        return sourceStation != 1 && destinationStation != 1 && sourceStation < destinationStation;
     }
 }
